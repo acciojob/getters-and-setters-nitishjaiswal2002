e7e89ffb116d9b5cf315912502288e8a409f2081
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-10-23 16:10:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(age ,name){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name; 
+	}
 
-class Student extends Person {}
+	set age(age){
+		if(age >=0){
+			this._age=age;
+		}else{
+			console.log("Age cannot be negative.");
+		}
+	}
+	get age(){
+		return this._age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this._name}is studying.`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this._name}is teaching.`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
