4379b344ad9e7498e760e8a76a6b0356ad20a044
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-25 15:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-//complete this code
 class Person {
-	constructor(name,age) {
-		this._name=name;
-		this._age=age;
-	}
-	get name(){
-		return this._name=name;
-	}
-	set age(){
-		this._age=age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
 }
 
-class student extends Person{
-	study(){
-		console.log(`${this._name}is studying`);
-	}
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
+
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.age}is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30;
-console.log(person.age); // Output: 30
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); 
-
-
